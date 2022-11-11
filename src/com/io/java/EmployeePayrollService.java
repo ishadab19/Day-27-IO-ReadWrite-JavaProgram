3aa8c7fb49797d800a86b92e74fd35178a5d140f
shadab <me.shadab19@gmail.com>
2022-11-11 13:00:16 +0530

    [Added - Creating file for storing emp payRoll service entries ]

--- src/com/io/java/EmployeePayrollService.java
+++ src/com/io/java/EmployeePayrollService.java
@@ -1,5 +1,4 @@
 package com.io.java;
-
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
@@ -21,41 +20,21 @@ import java.nio.file.WatchService;
 import java.util.ArrayList;
 import java.util.List;
 import static java.nio.file.StandardWatchEventKinds.*;
+
 public class EmployeePayrollService {
-
-	public void listFilesDirectory() throws IOException, InterruptedException  {
-		// Activate watch Service
-		WatchService watchService = FileSystems.getDefault().newWatchService();
-
-		// Convert location to file path
-		Path path = Paths.get("C:\\Users\\mesha\\OneDrive\\Desktop\\WatchService");
-
-		// Register watch service on above path ==> Pass events to be watched
-		// (Create/Delete/Modify)
-		path.register(watchService, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
-
-
-		List<String> logs = new ArrayList<String>();
-
-
-		boolean poll = true;
-		while (poll) {
-
-			// Reference / Pointer current file or folder watching
-			WatchKey key = watchService.take();
-
-			// Printing the events (Create / Modify / Delete)
-			for (WatchEvent<?> event : key.pollEvents()) {
-				System.out.println("Event kind : " + event.kind() + " - File : " + event.context());
-
-				logs.add("Event kind : " + event.kind() + " - File : " + event.context());
-			}
-
-			Files.write(Paths.get("C:\\Users\\mesha\\OneDrive\\Desktop\\WatchService\\"), logs, StandardOpenOption.CREATE);
 	
-			// Reseting the reference so that it can be used later
-			poll = key.reset();
-			}}
+	
+	public void listFilesDirectory() throws IOException, InterruptedException  {
+		ArrayList<String> writing = new ArrayList<String>();
+			
+			writing.add("Id : 01");
+			writing.add("Name : Shadab");
+			writing.add("Salary : 100000");
+//		File Exist => Override                        
+				// Will create new file if not exist                  
+				Files.write(Paths.get("C:\\Users\\mesha\\OneDrive\\Desktop\\DataBase\\Day-27-IO_ReadWrite_JavaProgram\\src\\com\\io\\java\\TestDemo.txt"), writing, StandardOpenOption.CREATE);
+	System.out.println("Exporting Into File Is Done...");
+	}
 public static void main(String[] args) throws IOException, InterruptedException {
 	EmployeePayrollService obj=new EmployeePayrollService();
 	obj.listFilesDirectory();
